ce300d5d8f92fe27e5cedfdb3e09005ba5041a3b
Adam Schubach <33351047+TheAtomicShoebox@users.noreply.github.com>
2025-03-17 02:40:49 +0000

    Basic debug for ticks, initial commit

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+
+#define TICKS_PER_SECOND 75.0
+#define SKIP_TICKS 1000000.0 / TICKS_PER_SECOND
+#define MAX_FRAMESKIP 5
+
+volatile sig_atomic_t stop;
+
+void interruptHandler(int signum)
+{
+	stop = 1;
+}
+
+
+int main(int argc, char *argv[])
+{
+	signal(SIGINT, interruptHandler);
+	clock_t next_tick = clock();
+
+	int loops;
+	
+
+	printf("Output: %f\n",(float)clock());
+	printf("Clocks per sec: %ld\n", CLOCKS_PER_SEC);
+	next_tick += SKIP_TICKS;
+	while(!stop)
+	{
+		loops = 0;
+		while(clock() > next_tick && loops < MAX_FRAMESKIP)
+		{
+			next_tick += SKIP_TICKS;
+			printf("next_tick: %ld\n", next_tick);
+			printf("clock(): %ld\n", clock());
+			loops++;
+			printf("loops: %d\n", loops);
+			printf("Game ticked\n");
+		}
+	}
+	printf("Game ended\n");
+	return 0;
+}
